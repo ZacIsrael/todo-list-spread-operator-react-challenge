00c02f04c5a2e845cd55901e81b8234dc9acbdba
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-18 21:58:55 -0400

    Implemented ToDo item functionality with state management.

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
+function ToDoItem(props) {
+  // Manage the state of the ToDo item. Default is set to false
+  // (the todo item has not been completed yet which means that it
+  // will not have a line through it)
+  const [isDone, setIsDone] = useState(false);
+
+  // executed once the ToDo item has been clicked
+  function itemClicked(event) {
+
+    setIsDone((prevValue) => {
+      // if it's true, set it to false & vice versa.
+      return !prevValue;
+    });
+  }
+
+  return (
+    // ternary operator will add or remove the line-through decoration depending on the value of the isDone boolean
+    <li onClick={itemClicked} style={{ textDecoration: isDone ? "line-through" : "none" }}>
+      {props.item}
+    </li>
+  );
+}
+
+
+
+/* 
+// Previous implementation (without state)
+
+// executed once the ToDo item has been clicked
 function itemClicked(event) {
   let { target } = event;
 
@@ -17,5 +46,6 @@ function itemClicked(event) {
 function ToDoItem(props) {
   return <li onClick={itemClicked}>{props.item}</li>;
 }
+*/
 
 export default ToDoItem;
