c064cad00c64c9eabf34a24c54f9d3d5ac5a93ea
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-18 21:34:31 -0400

    Added onClick function that adds and removes a line on a ToDo item once it has been clicked.

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+function itemClicked(event) {
+  let { target } = event;
+
+  console.log(`ToDoItem: target = `, target);
+
+  // add a line through the item if it has been clicked;
+  // remove the line if it's already there
+  if (target.style.textDecoration === "line-through") {
+    target.style.textDecoration = "";
+  } else {
+    target.style.textDecoration = "line-through";
+  }
+}
+
 function ToDoItem(props) {
-  return <li>{props.item}</li>;
+  return <li onClick={itemClicked}>{props.item}</li>;
 }
 
 export default ToDoItem;
